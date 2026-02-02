b65e7aa2da4d74ad54247a6f11073d81ff507b75
Mohammadreza <m.gohari@drdr.doctor>
2026-02-03 00:30:44 +0330

    setup project

--- src/Modules/User/Database/migrations/0001_01_01_000000_create_users_table.php
+++ src/Modules/User/Database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->string('mobile')->unique();
             $table->string('email')->unique();
+            $table->string('is_active')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -22,7 +24,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('mobile')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
